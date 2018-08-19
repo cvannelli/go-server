ca0c9700ef967d84d35e86453f7ed0680140f5cd
Chris <cvannelli123@gmail.com>
2018-08-18 23:44:30 -0700

    clean up

--- main.go
+++ main.go
@@ -21,30 +21,3 @@ func main() {
 	r.GET("/contents", handler.TestContents)
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
-// func main() {
-
-// 	client, err := mongo.NewClient("mongodb://foo:bar@localhost:27017")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	// server := server{db = }
-// 	// mux := httprouter.New()
-// 	// mux.GET("/", index)
-// 	// mux.GET("/about", about)
-// 	// mux.GET("/contact", contact)
-// 	// mux.GET("/apply", apply)
-// 	// mux.POST("/apply", applyProcess)
-// 	// mux.GET("/user/:name", user)
-// 	// mux.GET("/blog/:category/:article", blogRead)
-// 	// mux.POST("/blog/:category/:article", blogWrite)
-// 	// http.ListenAndServe(":8080", mux)
-// }
-
-// func HandleError(w http.ResponseWriter, err error) {
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		log.Fatalln(err)
-// 	}
-// }
